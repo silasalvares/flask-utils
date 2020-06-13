e92ef830e7a03b4ce0839ef5c87da2c0e06552b6
Silas Carvalho <silasalvares@gmail.com>
2020-06-13 11:39:34 -0300

    changing error handler

--- flask_utils/functions.py
+++ flask_utils/functions.py
@@ -5,16 +5,13 @@ def make_json_webapi(app_name, **kwargs):
     
     def make_json_error(ex):
         error = str(ex)
-        if not str(error):
-            error = 'Internal Server Error' 
-        error += ' ' + str(type(ex))
         return make_default_response(success=False, errors=[error])
 
     flask_app = Flask(app_name)
     for exception in default_exceptions.items():
         flask_app.register_error_handler(exception[0], make_json_error)
     
-    flask_app.register_error_handler(Exception, make_json_error), ex.code
+    flask_app.register_error_handler(Exception, make_json_error)
 
     @flask_app.after_request
     def set_content_type(response):
